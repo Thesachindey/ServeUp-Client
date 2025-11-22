970ae54662313ee426b4594f959d04f25625f9ec
Sachin <sachinscode24@gmail.com>
2025-11-22 12:17:12 +0600

    added /joined-events api end point

--- src/Components/JoinedEventCard.jsx
+++ src/Components/JoinedEventCard.jsx
@@ -20,7 +20,7 @@ const handleDelete = () => {
     confirmButtonText: "Yes, delete it!",
   }).then((result) => {
     if (result.isConfirmed) {
-      fetch(`http://localhost:3000/events/${event._id}`, {
+      fetch(`http://localhost:3000/joined-events/${event._id}`, {
         method: "DELETE"
       })
         .then((res) => res.json())
